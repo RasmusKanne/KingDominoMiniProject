2b0018d77cd3f72b4564db3d9e0442af3a1d85de
RasmusKanne <rasmuskanne@gmail.com>
2022-10-13 11:02:42 +0200

    Inbuilt Template Matching test

--- Main.py
+++ Main.py
@@ -1,3 +1,13 @@
 import cv2 as cv
 import numpy as np
 
+inputPic = cv.imread("1.jpg")
+
+input_gray = cv.cvtColor(inputPic, cv.COLOR_BGR2GRAY)
+template = cv.imread("CrownTemplate.png", 0)
+
+res = cv.matchTemplate(input_gray,template,cv.TM_CCORR_NORMED)
+
+cv.imshow("Correlation", res)
+cv.imshow("Input Image", inputPic)
+cv.waitKey(0)
