3a7d7248dd3fd514f3510d90531eb1ff0020a575
RasmusKanne <rasmuskanne@gmail.com>
2022-11-21 10:22:02 +0100

    Template matching updated

--- Main.py
+++ Main.py
@@ -2,9 +2,10 @@ import cv2 as cv
 import numpy as np
 from Slicing import slicing
 from Categorization import FindValue, Categorize
+from TemplateMatching import TemplateMatching, RunNonMaxima
 
 # Read Image
-inputPic = cv.imread("King Domino dataset/Cropped and perspective corrected boards/10.jpg")
+inputPic = cv.imread("King Domino dataset/Cropped and perspective corrected boards/1.jpg")
 
 # Convert and split HSV channels
 HSV = cv.cvtColor(inputPic, cv.COLOR_BGR2HSV)
@@ -26,8 +27,31 @@ h_rows_values = FindValue(h_rows)
 s_rows_values = FindValue(s_rows)
 v_rows_values = FindValue(v_rows)
 
-# Takes in the HSV values and puts them through if statements to categorize all terrain blocks into the 6 different types
+# Takes in the HSV values
+# then puts them through if statements to categorize all terrain blocks into the 6 different types
 c_rows = Categorize(h_rows_values, s_rows_values, v_rows_values)
+
+# Finds amount of crowns using template matching
+# Runs it 4 times for all 4 states of rotation the crowns are able to be in
+template = cv.imread("CrownTemplateResize.png")
+template_90 = cv.rotate(template, cv.ROTATE_90_CLOCKWISE)
+template_180 = cv.rotate(template_90, cv.ROTATE_90_CLOCKWISE)
+template_270 = cv.rotate(template_180, cv.ROTATE_90_CLOCKWISE)
+
+rect1 = TemplateMatching(inputPic, template, 0.55)
+rect2 = TemplateMatching(inputPic, template_90, 0.55)
+rect3 = TemplateMatching(inputPic, template_180, 0.55)
+rect4 = TemplateMatching(inputPic, template_270, 0.55)
+
+# Then all coordinates for crowns from the 4 template matches are put into 1 list
+rects = [rect1, rect2, rect3, rect4]
+all_rects = []
+for i in range(len(rects)):
+    for z in range(len(rects[i])):
+        all_rects.append(rects[i][z])
+
+# Then run through a non maxima supression algorithm to remove any overlapping results.
+matching_result, matching_image = RunNonMaxima(inputPic, all_rects)
 
 print(f"Hue: {h_rows_values}")
 print(f"Saturation: {s_rows_values}")
@@ -36,4 +60,5 @@ print(f"Value: {v_rows_values}")
 print(c_rows)
 
 cv.imshow("Input", inputPic)
+cv.imshow("template matching", matching_image)
 cv.waitKey(0)
--- TemplateMatching.py
+++ TemplateMatching.py
@@ -1,25 +1,48 @@
 import cv2 as cv
 import numpy as np
+from NonMaximaSupression import non_max_suppression
 
 # Read Image
-inputPic = cv.imread("King Domino dataset/Cropped and perspective corrected boards/1.jpg")
+# inputPic = cv.imread("King Domino dataset/Cropped and perspective corrected boards/1.jpg")
 
 # Converting the Image
-input_gray = cv.cvtColor(inputPic, cv.COLOR_BGR2GRAY)
+# input_gray = cv.cvtColor(inputPic, cv.COLOR_BGR2GRAY)
 
 # Template Matching To Find Crowns
-template = cv.imread("CrownTemplateResize.png", 0)
+# template = cv.imread("CrownTemplateResize.png", 0)
 
-w, h = template.shape[::-1]
+def TemplateMatching(processed, template, thresholdValue):
+    #image_gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+    processed = np.array(processed, dtype=np.uint8)
+    template = np.array(template, dtype=np.uint8)
+    tH, tW = template.shape[:2]
 
-res = cv.matchTemplate(input_gray,template,cv.TM_CCOEFF_NORMED)
-threshold = 0.65
-loc = np.where( res >= threshold)
-for pt in zip(*loc[::-1]):
-    cv.rectangle(inputPic, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
+    res = cv.matchTemplate(processed, template, cv.TM_CCOEFF_NORMED)
+    threshold = thresholdValue
+    (yCoords, xCoords) = np.where(res >= threshold)
+
+    rects = []
+    for (x, y) in zip(xCoords, yCoords):
+        rects.append((x, y, x + tW, y + tH))
+
+    # show the output image
+    return rects
+
+def RunNonMaxima(image, rects):
+    result = image.copy()
+    pick = non_max_suppression(np.array(rects), 0.1)
+    resultAmount = len(pick)
+    print(resultAmount)
+
+    # loop over the final bounding boxes
+    for (startX, startY, endX, endY) in pick:
+        # draw the bounding box on the image
+        cv.rectangle(result, (startX, startY), (endX, endY),
+                      (0, 0, 255), 3)
+    return pick, result
 
 # Showing the Result
-cv.imwrite('res.png',inputPic)
-cv.imshow("Correlation", res)
-cv.imshow("Input Image", inputPic)
-cv.waitKey(0)
+# cv.imwrite('res.png',inputPic)
+# cv.imshow("Correlation", res)
+# cv.imshow("Input Image", inputPic)
+# cv.waitKey(0)
